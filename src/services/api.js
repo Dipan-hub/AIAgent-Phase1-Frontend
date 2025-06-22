e6b6bd1ce954a40659b55c745890fa1eb574267c
Dipan <ms21btech11021@iith.ac.in>
2025-06-22 22:34:44 +0530

    Changed Base Url

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://aiagent-env.eba-vvwqbxfg.us-east-1.elasticbeanstalk.com";
+//const BASE_URL = "https://aiagent-env.eba-vvwqbxfg.us-east-1.elasticbeanstalk.com";
+const BASE_URL = " https://aiagent-phase1-backend.onrender.com ";
 
 export const sendPrompt = async (prompt) => {
   try {
